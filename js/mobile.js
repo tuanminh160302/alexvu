0fa69a0c9560a54c9c99e19c836e3958f022254c
tuanminh160302 <51919628+tuanminh160302@users.noreply.github.com>
2020-12-23 13:53:23 +0700

    update

--- js/mobile.js
+++ js/mobile.js
@@ -1,8 +1,10 @@
 document.getElementById('menu-trigger').addEventListener('click', ()=> {
     TweenMax.to('#menu-trigger', .5, { scaleX: 0});
+    TweenMax.to('#menu-trigger-container', .5, { opacity: 0});
     TweenMax.to('.menu', 1.2, { delay: .3, scaleX: 1, ease: Power4.easeInOut});
     TweenMax.to('.html', 0, { overflowY: 'hidden'});
     TweenMax.to('#menu-trigger', 0, { delay: .5, display: 'none'});
+    TweenMax.to('#menu-trigger-container', 0, { delay: .5, display: 'none'});
     TweenMax.to('#close-trigger', 0, { delay: 1.5, display: 'block'});
     TweenMax.to('#close-trigger', .5, { delay: 1.5, opacity: 1});
     TweenMax.to('.menu-animated', .5, { opacity: 0});
@@ -17,9 +19,138 @@ document.getElementById('close-trigger').addEventListener('click', ()=> {
     TweenMax.to('.html', 0, { delay: 1.5, overflowY: 'auto'});
     TweenMax.to('#close-trigger', 0, { delay: .5, display: 'none'});
     TweenMax.to('#menu-trigger', 0, { delay: 1.5, display: 'block'});
+    TweenMax.to('#menu-trigger-container', 0, { delay: 1.5, display: 'flex'});
     TweenMax.to('#menu-trigger', .5, { delay: 1.5, scaleX: 1});
+    TweenMax.to('#menu-trigger-container', .5, { delay: 1.5, opacity: 1});
     TweenMax.to('.menu-animated', .5, { delay: 1.5, opacity: 1});
     TweenMax.to('.socials', 0, { delay: .75, webkitFilter: 'invert(0)'});
     
     TweenMax.to('.prj-title', 1.5, { x: '-50vw', ease: Power4.easeInOut});
+})
+
+let i = 1;
+
+document.getElementById('next-img').addEventListener('click', ()=> {
+    TweenMax.to('#img-slider-curtain', 0, { transformOrigin: 'right'});
+    TweenMax.to('#img-slider-curtain', 1.1, { scaleX: 1, ease: Power4.easeInOut});
+    TweenMax.to('#img-slider-curtain', 0, { delay: 1.1, transformOrigin: 'left'});
+    TweenMax.to('#img-slider-curtain', 1.1, { delay: 1.5, scaleX: 0, ease: Power4.easeInOut});
+
+    i++
+    setTimeout(()=> {
+        document.getElementById('img-slider').src = `/img/img-slider/${i}.jpg`
+    },1100);
+    if (i > 10) {
+        i == 10;
+        TweenMax.to('#next-img', 0, { delay: 1.1, display: 'none'});
+    } else if (i == 10) {
+        TweenMax.to('#next-img', 0, { delay: 1.1, display: 'none'});
+    } else {
+        TweenMax.to('#prev-img', 0, { delay: 1.1, display: 'block'});
+    }
+
+    if (i==1) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'red';
+        }, 1100)
+    } else if (i==2) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'beige';
+        }, 1100)
+    } else if (i==3) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'black';
+        }, 1100)
+    } else if (i==4) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'green';
+        }, 1100)
+    } else if (i==5) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'magenta';
+        }, 1100)
+    } else if (i==6) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'marble';
+        }, 1100)
+    } else if (i==7) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'pink';
+        }, 1100)
+    } else if (i==8) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'purple';
+        }, 1100)
+    } else if (i==9) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'rainbow';
+        }, 1100)
+    } else if (i==10) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'white';
+        }, 1100)
+    }
+})
+
+document.getElementById('prev-img').addEventListener('click', ()=> {
+    TweenMax.to('#img-slider-curtain', 0, { transformOrigin: 'left'});
+    TweenMax.to('#img-slider-curtain', 1.1, { scaleX: 1, ease: Power4.easeInOut});
+    TweenMax.to('#img-slider-curtain', 0, { delay: 1.1, transformOrigin: 'right'});
+    TweenMax.to('#img-slider-curtain', 1.1, { delay: 1.5, scaleX: 0, ease: Power4.easeInOut});
+
+
+    i--
+    setTimeout(()=> {
+        document.getElementById('img-slider').src = `/img/img-slider/${i}.jpg`
+    },1100);
+    if (i < 1) {
+        i == 1;
+        TweenMax.to('#prev-img', 0, { delay: 1.1, display: 'none'});
+    } else if (i == 1) {
+        TweenMax.to('#prev-img', 0, { delay: 1.1, display: 'none'});
+    } else {
+        TweenMax.to('#next-img', 0, { delay: 1.1, display: 'block'});
+    }
+
+    if (i==1) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'red';
+        }, 1100)
+    } else if (i==2) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'beige';
+        }, 1100)
+    } else if (i==3) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'black';
+        }, 1100)
+    } else if (i==4) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'green';
+        }, 1100)
+    } else if (i==5) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'magenta';
+        }, 1100)
+    } else if (i==6) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'marble';
+        }, 1100)
+    } else if (i==7) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'pink';
+        }, 1100)
+    } else if (i==8) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'purple';
+        }, 1100)
+    } else if (i==9) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'rainbow';
+        }, 1100)
+    } else if (i==10) {
+        setTimeout(()=> {
+            document.getElementById('img-title').innerHTML = 'white';
+        }, 1100)
+    }
 })
--- js/scrollMagic.js
+++ js/scrollMagic.js
@@ -1,9 +1,37 @@
 var controller = new ScrollMagic.Controller();
 
 var scene = new ScrollMagic.Scene({
-    triggerElement: '#intro-avt',
-    triggerHook: 0.2,
+    triggerElement: '#intro-p',
+    triggerHook: 0.1,
 })
 scene.addIndicators();
 scene.setTween('#intro-avt', .5, { opacity: 0});
 scene.addTo(controller);
+
+
+
+
+
+
+
+
+var lastScrollTop = 0;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+       //downscroll code
+      TweenMax.to('.header', .8, { scaleY: 1, ease: Power4.easeOut});
+
+      TweenMax.to('.scroll-opacity', .3, {opacity: 0})
+      TweenMax.to('#img-title-container', .8, { scaleY: 0, ease: Power4.easeOut});
+   } else {
+       //upscroll code
+      TweenMax.to('.header', .8, { scaleY: 0, ease: Power4.easeOut});
+
+      TweenMax.to('.scroll-opacity', .5, {delay: .4, opacity: 1})
+      TweenMax.to('#img-title-container', .8, { scaleY: 1, ease: Power4.easeOut});
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
